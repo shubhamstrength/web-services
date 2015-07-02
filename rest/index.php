b9114d374f04f012392416073680815663b9ee43
shubhamstrength <shubhamstrength@gmail.com>
2015-07-02 13:34:20 +0530

    Create index.php

---
+++ rest/index.php
@@ -0,0 +1,26 @@
+<?php
+header("Content-Type:application/json");
+include("functions.php");
+if(!empty($_GET['name'])){
+$name = $_GET['name'];
+$price= get_price($name);
+if(empty($price))
+{
+deliver_response(200,"book not found",NULL);
+}
+else {
+	deliver_response(200,"book found",$name);
+}}
+else{
+//throw invalid request
+deliver_response(400,'Invalid request',null);
+}
+function deliver_response($status,$status_message,$data){
+header("HTTP/1.1 $status.$status_message");
+$response ['status']=$status;
+$response ['status_message']=$status_message;
+$response ['data'] = $data;
+$json_response = json_encode($response);
+echo $json_response;
+}
+?>
